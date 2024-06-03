505a6b27db1bb99ea11d482fa610f6e6453a5b19
GaneshSawatee <163160413+GaneshSawatee@users.noreply.github.com>
2024-06-03 17:20:59 +0530

    Add files via upload

---
+++ If-else.py
@@ -0,0 +1,54 @@
+# equal (==)
+
+a = 70
+
+b = 70
+
+if a == b:
+    print('a is equal to b')
+else:
+    print('a is not equal to b')
+
+# Greater than ( > )
+
+x = 1220
+
+y =120
+
+if x >= y:
+    print('x is greater than y')
+else:
+    print('x is not greater than y')
+
+
+# Less than ( < )
+x = 10
+
+y =120
+
+if x <= y:
+    print('x is less than y')
+else:
+    print('x is not less than y')
+
+# user input
+
+
+val1 =int(input("Enter val1: "))
+val2 =int(input("Enter val2: "))
+print('Entered val1 is {} and val2 is {} \n The result is '. format(val1,val2))
+if val1 >= val2:
+    print('val1 is greater than val2')
+else:
+    print('val1 is not greater than val2')
+
+
+
+age = int(input("Enter age: "))
+
+print('Age is ', age)
+
+if age >= 18:
+    print(' you are eligible for vote')
+else:
+    print(' you are not eligible for vote')
---
+++ elif.py
@@ -0,0 +1,30 @@
+num = int(input('Enter nummber between 1 to 5 : '))
+
+print('your number is : ', num)
+
+if num == 1:
+    print('You Select Option A')
+elif num == 2:  
+    print('You Select Option B')
+elif num == 3:
+    print('You Select Option C')
+elif num == 4:
+    print('You Select Option D')
+elif num == 5:
+    print('You Select Option E')
+else:
+    print('You Select wrong Option')
+
+
+
+name = input("Enter fruits or vegetable name :")
+if name=="onion":
+  print("🧅")
+elif name=="apple":
+  print("🍎")
+elif name=="banana":
+  print("🍌")
+elif name=="pineabpple":
+  print("🍍")
+else:
+  print("Not avilable")
